6ecdcea2af11e891efb023b33763ff64133c860d
Noa Kuperberg <noakup@gmail.com>
2015-03-26 19:27:04 +0200

    changes FileUserstore to SimpleUserstore (name and implementation)

--- flask_securest/userstores/simple.py
+++ flask_securest/userstores/simple.py
@@ -11,19 +11,14 @@ ROLES = 'roles'
 RESOURCES_PATH = os.path.dirname(__file__) + '/../resources'
 
 
-class FileUserstore(AbstractUserstore):
+class SimpleUserstore(AbstractUserstore):
 
-    def __init__(self, userstore_file, identifying_attribute):
-        print '***** INITING class FileUserstore with identifying_attr {0}' \
+    def __init__(self, userstore, identifying_attribute):
+        print '***** INITING class SimpleUserstore with identifying_attr {0}' \
               ' and file_location {1}'.format(identifying_attribute,
-                                              userstore_file)
+                                              userstore)
         self._identifying_attribute = identifying_attribute
-        abs_file_location = RESOURCES_PATH + "/" + userstore_file
-        if not os.path.isfile(abs_file_location):
-            raise ValueError('file not found: {0}'.format(abs_file_location))
-
-        with open(abs_file_location) as users_file:
-            self.users = yaml.load(users_file.read())
+        self.users = userstore
 
     def get_user(self, user_identifier):
         user_obj = None
@@ -38,17 +33,17 @@ class FileUserstore(AbstractUserstore):
             if user_entry.get(self._identifying_attribute) == user_identifier:
                 print '***** found user!'
                 # a matching user was found, return as a User object
-                user_obj = FileUserstore._create_user_object(user_entry)
+                user_obj = SimpleUserstore._create_user_object(user_entry)
                 break
 
         return user_obj
 
     @staticmethod
     def _create_user_object(user_dict):
-
         roles = []
-        for role_name in user_dict[ROLES]:
-            roles.append(Role(role_name))
 
+        if ROLES in user_dict:
+            for role_name in user_dict[ROLES]:
+                roles.append(Role(role_name))
         return User(user_dict[USERNAME], user_dict[PASSWORD],
                     user_dict[EMAIL], roles, active=True)
