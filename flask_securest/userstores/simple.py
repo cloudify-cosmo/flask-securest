14c253fa15fc5cfc7c30eea0a662557f76614e61
Noa Kuperberg <noakup@gmail.com>
2015-03-31 16:21:44 +0300

    removed debug printouts

--- flask_securest/userstores/simple.py
+++ flask_securest/userstores/simple.py
@@ -28,24 +28,17 @@ RESOURCES_PATH = os.path.dirname(__file__) + '/../resources'
 class SimpleUserstore(AbstractUserstore):
 
     def __init__(self, userstore, identifying_attribute):
-        print '***** INITING class SimpleUserstore with identifying_attr {0}' \
-              ' and file_location {1}'.format(identifying_attribute,
-                                              userstore)
         self._identifying_attribute = identifying_attribute
         self.users = userstore
 
     def get_user(self, user_identifier):
         user_obj = None
 
-        print '***** getting user where {0} = {1}'\
-            .format(self._identifying_attribute, user_identifier)
-
         if not user_identifier:
             raise ValueError('user identifier is missing or empty')
 
         for user_entry in self.users.itervalues():
             if user_entry.get(self._identifying_attribute) == user_identifier:
-                print '***** found user!'
                 # a matching user was found, return as a User object
                 user_obj = SimpleUserstore._create_user_object(user_entry)
                 break
