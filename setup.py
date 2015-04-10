c2a47a51b90b174efda38e393ce03e936ab43ea2
noakup <noak@gigaspaces.com>
2015-04-10 18:43:37 +0300

    added dev status classifier

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        # 'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
