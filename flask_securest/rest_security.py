fb459e238c2c8af5ecb76040c7be648268f8b870
Noa Kuperberg <noakup@gmail.com>
2015-04-27 12:08:22 +0300

    CFY-2535 - removed ip resolving to avoid blocking

--- flask_securest/rest_security.py
+++ flask_securest/rest_security.py
@@ -200,12 +200,7 @@ def get_auth_info_from_request():
 def get_request_origin():
     request_origin_ip = request.remote_addr
     if request_origin_ip:
-        try:
-            request_origin_host = utils.get_hostname_by_ip(request_origin_ip)
-            request_origin = '[{0}/{1}]'.format(request_origin_host,
-                                                request_origin_ip)
-        except:
-            request_origin = '[{0}]'.format(request_origin_ip)
+        request_origin = '[{0}]'.format(request_origin_ip)
     else:
         request_origin = '[unknown]'
     return request_origin
