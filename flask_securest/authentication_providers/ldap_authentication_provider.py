b4171b0efd6f4bb51abd471a942adfbb18208afc
adamlavie <adaml@gigaspaces.com>
2015-11-21 21:20:43 +0200

    removed unused import

--- flask_securest/authentication_providers/ldap_authentication_provider.py
+++ flask_securest/authentication_providers/ldap_authentication_provider.py
@@ -17,7 +17,6 @@
 import ldap
 from flask_securest.authentication_providers.abstract_authentication_provider \
     import AbstractAuthenticationProvider
-from flask.ext.securest.models import User
 
 
 class LDAPAuthenticationProvider(AbstractAuthenticationProvider):
